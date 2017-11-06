61b101acd5d735e68eb6b80e6483bc42c6c5dae2
Training_b6b.01.09 <Training_b6b.01.09@BDC6-DX-84JMX52.dir.svc.accenture.com>
2017-11-06 16:52:41 +0530

    dddddddddddd

---
+++ src/Employee.java
@@ -0,0 +1,21 @@
+
+public class Employee {
+	
+	private String EmpName;
+	private int Empid;
+	public String getEmpName() {
+		return EmpName;
+	}
+	public void setEmpName(String empName) {
+		EmpName = empName;
+	}
+	public int getEmpid() {
+		return Empid;
+	}
+	public void setEmpid(int empid) {
+		Empid = empid;
+	}
+	
+	
+
+}
